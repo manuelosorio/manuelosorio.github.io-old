fe33658f53f2384a5209a9667a1e592d02ea5320
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2015-07-27 12:13:31 -0400

    Add Mobile nav and Add rouge properly

---
+++ assets/js/toggleNav.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+
+    //when nav icon is clicked if the menu is active remove class active otherwise add class active to nToggle.
+    $('.icon-navicon').click(function () {
+
+        $('.mobile-nav').toggleClass('active');
+    });
+
+});
